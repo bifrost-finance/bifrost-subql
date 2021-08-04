32aa372ec3105f06ffecca7af9b8370379bf6419
yooml <ymlll0508@gmail.com>
2021-08-04 10:43:49 +0800

    fix: 🐛 RPC call succeeded

--- src/mappings/tests.ts
+++ src/mappings/tests.ts
@@ -1,12 +1,16 @@
 import { SubstrateBlock } from "@subql/types";
+import {PairInfo} from "bifrost-subql/api-interfaces";
+import {Vec} from "@polkadot/types";
 
 export async function rpcTest(block: SubstrateBlock): Promise<void> {
   logger.info('======');
   const b2 = await api.rpc.chain.getBlock();
   logger.info(b2.block.header.number.toNumber());
-  // let b = await api.rpc.zenlinkProtocol.getAllAssets();
-  // logger.info(b[0].chain_id);
-  let c = await api.rpc.rpc.methods();
-  logger.info(c);
+  let pairs = await api.rpc.zenlinkProtocol.getAllPairs() as Vec<PairInfo>;
+  for (const pair of pairs){
+    logger.info(`pair: ${pair.toString()}`);
+  }
+  // let c = await api.rpc.rpc.methods();
+  // logger.info(c);
   logger.info('------');
 }
