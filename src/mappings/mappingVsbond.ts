354dca55ff568a7caa878864dadd9a652abb6980
yooml <947801604@qq.com>
2021-12-07 10:55:15 +0800

    add OrderClinchd

--- src/mappings/mappingVsbond.ts
+++ src/mappings/mappingVsbond.ts
@@ -1,7 +1,9 @@
 import { SubstrateBlock, SubstrateEvent } from "@subql/types";
-import { BlockNumber } from "@polkadot/types/interfaces";
+import { BlockNumber, Balance } from "@polkadot/types/interfaces";
 import { Compact } from '@polkadot/types';
-import { VsbondInfo } from "../types/models/VsbondInfo";
+import BigNumber from "bignumber.js";
+import { VsbondInfo, VsBondOrderClinchd } from "../types/models";
+import { getPrice } from "../common";
 
 export async function vsbond(block: SubstrateBlock): Promise<void> {
   const blockNumber = (block.block.header.number as Compact<BlockNumber>).toBigInt();
@@ -17,4 +19,23 @@ export async function vsbond(block: SubstrateBlock): Promise<void> {
     await record.save();
   }
   return;
+}
+
+export async function handleVsBondAuctionOrderClinchd(event: SubstrateEvent): Promise<void> {
+  const blockNumber = event.block.block.header.number.toNumber();
+
+  const kusama_price = await getPrice(event.block, 'kusama');
+  const { event: { section, method, data: [order_id, order_type, order_creator, order_opponent, vsbond_type,
+    vsbond_amount_clinched, vsbond_amount, vsbond_remain, total_price] } } = event;
+  const record = new VsBondOrderClinchd(blockNumber.toString() + '-' + event.idx.toString());
+  record.block_height = blockNumber;
+  record.event_id = event.idx;
+  record.extrinsic_id = event.extrinsic ? event.extrinsic.idx : null;
+  record.block_timestamp = event.block.timestamp;
+  record.currency = vsbond_type.toString();
+  record.total_price = (total_price as Balance).toBigInt();
+  record.amount = (vsbond_amount as Balance).toBigInt();
+  const price = new BigNumber(kusama_price.usd).multipliedBy(record.total_price.toString()).div(record.amount.toString());
+  record.price = parseFloat(price.toFixed(8));
+  await record.save();
 }
