89b0b4573fd829d7c27f4c50f2332980390a04d2
yooml <947801604@qq.com>
2021-12-09 15:55:26 +0800

    fix vsbond

--- src/mappings/mappingVsbond.ts
+++ src/mappings/mappingVsbond.ts
@@ -24,7 +24,7 @@ export async function vsbond(block: SubstrateBlock): Promise<void> {
 export async function handleVsBondAuctionOrderClinchd(event: SubstrateEvent): Promise<void> {
   const blockNumber = event.block.block.header.number.toNumber();
 
-  const kusama_price = await getPrice(event.block, 'kusama');
+  // const kusama_price = await getPrice(event.block, 'kusama');
   const { event: { section, method, data: [order_id, order_type, order_creator, order_opponent, vsbond_type,
     vsbond_amount_clinched, vsbond_amount, vsbond_remain, total_price] } } = event;
   const record = new VsBondOrderClinchd(blockNumber.toString() + '-' + event.idx.toString());
@@ -35,7 +35,8 @@ export async function handleVsBondAuctionOrderClinchd(event: SubstrateEvent): Pr
   record.currency = vsbond_type.toString();
   record.total_price = (total_price as Balance).toBigInt();
   record.amount = (vsbond_amount as Balance).toBigInt();
-  const price = new BigNumber(kusama_price.usd).multipliedBy(record.total_price.toString()).div(record.amount.toString());
+  // const price = new BigNumber(kusama_price.usd).multipliedBy(record.total_price.toString()).div(record.amount.toString());
+  const price = new BigNumber(record.total_price.toString()).div(record.amount.toString());
   record.price = parseFloat(price.toFixed(8));
   await record.save();
 }
