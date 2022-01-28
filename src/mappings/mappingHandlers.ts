6eb9c50caa3ba96de731bd840ef2b155240d015f
yooml <ymlll0508@gmail.com>
2022-01-28 19:34:53 +0800

    feat: 🎸 add democracy and council

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -3,7 +3,7 @@ import { StarterEntity } from "../types/models/StarterEntity";
 import { Balance, Moment } from "@polkadot/types/interfaces";
 import { SubstrateBlock } from "@subql/types";
 import { getPrice } from "../common";
-import { Tvl, Extrinsic } from '../types/models';
+import { Tvl, Extrinsic, DemocracyInfo, CouncilInfo } from '../types/models';
 import BigNumber from "bignumber.js";
 
 const Tokens = [
@@ -161,4 +161,36 @@ export async function handleSignedBlock(block: SubstrateBlock): Promise<void> {
       await record.save();
     }
   }));
+}
+
+export async function democracy(block: SubstrateBlock): Promise<void> {
+  const blockNumber = block.block.header.number.toNumber();
+  const democracyEvents = block.events.filter(e => (e.event.section === 'democracy')) as SubstrateEvent[];
+  for (let democracyEvent of democracyEvents) {
+    const { event: { data, section, method } } = democracyEvent;
+    const record = new DemocracyInfo(blockNumber.toString() + '-' + democracyEvent.idx.toString());
+    record.event_id = democracyEvent.idx;
+    record.extrinsic_id = democracyEvent.extrinsic ? democracyEvent.extrinsic.idx : null;
+    record.block_height = blockNumber;
+    record.block_timestamp = block.timestamp;
+    record.method = method.toString();
+    record.data = data.toString();
+    await record.save();
+  }
+}
+
+export async function council(block: SubstrateBlock): Promise<void> {
+  const blockNumber = block.block.header.number.toNumber();
+  const councilEvents = block.events.filter(e => (e.event.section === 'council')) as SubstrateEvent[];
+  for (let councilEvent of councilEvents) {
+    const { event: { data, section, method } } = councilEvent;
+    const record = new CouncilInfo(blockNumber.toString() + '-' + councilEvent.idx.toString());
+    record.event_id = councilEvent.idx;
+    record.extrinsic_id = councilEvent.extrinsic ? councilEvent.extrinsic.idx : null;
+    record.block_height = blockNumber;
+    record.block_timestamp = block.timestamp;
+    record.method = method.toString();
+    record.data = data.toString();
+    await record.save();
+  }
 }
