0e7ac35a5b22a43a6a184a48e94ef1225878a096
killbond007 <chenchengalex2014@gmail.com>
2022-04-29 06:27:29 +0200

    feat:Add success true for transfer

--- src/mappings/mappingVKSMTransfers.ts
+++ src/mappings/mappingVKSMTransfers.ts
@@ -16,41 +16,45 @@ export async function handleCurrenciesTransferred(
   const transferEvent = events.find(
     (e) => e.event.section === "currencies" && e.event.method === "Transferred"
   ) as SubstrateEvent;
-  const {
-    event: {
-      data: [currency, from, to, balance],
-    },
-  } = transferEvent;
 
-  if (currency.toString() === '{"vToken":"KSM"}') {
-    const vKSMtotalIssuance = await api.query.tokens?.totalIssuance({
-      vToken: "KSM",
-    });
-    const KSMTokenPool = await api.query.vtokenMinting?.tokenPool({
-      Token: "KSM",
-    });
+  if (transferEvent) {
+    const {
+      event: {
+        data: [currency, from, to, balance],
+      },
+    } = transferEvent;
 
-    const record = new VtokenTransfer(
-      blockNumber.toString() + "-" + transferEvent.idx.toString()
-    );
-    record.block_height = blockNumber;
-    record.block_timestamp = transferEvent.block.timestamp;
-    record.from_account = from.toString();
-    record.to_account = to.toString();
-    record.currency = currency.toString();
-    record.balance = (balance as Balance).toBigInt();
-    record.vksm_balance = vKSMtotalIssuance
-      ? (vKSMtotalIssuance as Balance).toBigInt()
-      : BigInt(0);
-    record.ksm_balance = KSMTokenPool
-      ? (KSMTokenPool as Balance).toBigInt()
-      : BigInt(0);
-    record.ratio =
-      KSMTokenPool?.toString() === "0" || vKSMtotalIssuance?.toString() === "0"
-        ? "0"
-        : new BigNumber(vKSMtotalIssuance?.toString())
-            .div(KSMTokenPool?.toString())
-            .toString();
-    await record.save();
+    if (currency.toString() === '{"vToken":"KSM"}') {
+      const vKSMtotalIssuance = await api.query.tokens?.totalIssuance({
+        vToken: "KSM",
+      });
+      const KSMTokenPool = await api.query.vtokenMinting?.tokenPool({
+        Token: "KSM",
+      });
+
+      const record = new VtokenTransfer(
+        blockNumber.toString() + "-" + transferEvent.idx.toString()
+      );
+      record.block_height = blockNumber;
+      record.block_timestamp = transferEvent.block.timestamp;
+      record.from_account = from.toString();
+      record.to_account = to.toString();
+      record.currency = currency.toString();
+      record.balance = (balance as Balance).toBigInt();
+      record.vksm_balance = vKSMtotalIssuance
+        ? (vKSMtotalIssuance as Balance).toBigInt()
+        : BigInt(0);
+      record.ksm_balance = KSMTokenPool
+        ? (KSMTokenPool as Balance).toBigInt()
+        : BigInt(0);
+      record.ratio =
+        KSMTokenPool?.toString() === "0" ||
+        vKSMtotalIssuance?.toString() === "0"
+          ? "0"
+          : new BigNumber(vKSMtotalIssuance?.toString())
+              .div(KSMTokenPool?.toString())
+              .toString();
+      await record.save();
+    }
   }
 }
