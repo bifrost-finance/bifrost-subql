d71a92c7897b1ea3b085cc9e2dff304d628957b7
killbond007 <chenchengalex2014@gmail.com>
2022-07-13 10:21:46 +0200

    feat: Add mappingCall

---
+++ src/mappings/mappingCall.ts
@@ -0,0 +1,33 @@
+import { SubstrateExtrinsic, SubstrateEvent } from "@subql/types";
+import { BlockNumber } from "@polkadot/types/interfaces";
+import { Compact } from "@polkadot/types";
+
+import { CallExtrinsic } from "../types";
+
+export async function handleCallExtrinsic(
+  extrinsic: SubstrateExtrinsic
+): Promise<void> {
+  const { block, events } = extrinsic;
+  const blockNumber = (
+    block.block.header.number as Compact<BlockNumber>
+  ).toBigInt();
+
+  const balancesWithdrawEvent = events.find(
+    (e) => e.event.section === "balances" && e.event.method === "Withdraw"
+  ) as SubstrateEvent;
+  if (balancesWithdrawEvent) {
+    const {
+      event: {
+        data: [account, _],
+      },
+    } = balancesWithdrawEvent;
+    const record = new CallExtrinsic(
+      blockNumber.toString() + "-" + extrinsic.idx.toString()
+    );
+    record.block_height = blockNumber;
+    record.block_timestamp = balancesWithdrawEvent.block.timestamp;
+    record.account = account.toString();
+
+    await record.save();
+  }
+}
