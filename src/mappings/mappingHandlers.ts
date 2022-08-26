5ed4cab0c56c64fb18758b38c1b1f840e8a6fb09
killbond007 <chenchengalex2014@gmail.com>
2022-08-26 07:50:14 +0200

    fix:Fix dex tvl

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -37,23 +37,16 @@ const Tokens = [
 
 const DexTokens = [
   {
-    id: "vsKSM-dex",
-    coin_id: "kusama",
-    currency: { vsToken: "KSM" },
-    decimal: 12,
-    dex_address: [
-      ["5EYCAe5ViNAoHnU1ZZAMeuwFRzWVSR8nXc92oKzfVm5Mqh7Q", { vsToken: "KSM" }],
-    ],
-  },
-  {
     id: "KSM-dex",
     coin_id: "kusama",
     currency: { Token: "KSM" },
     decimal: 12,
     dex_address: [
-      ["5EYCAe5ViNAoHnU1ZZAMeuwFRzWVSR8nXc92oKzfVm5Mqh7Q", { Token: "KSM" }],
-      ["5EYCAe5ViNAoHnU1ZZAR51LmTJsa1PC4JmRES1LEWhhomrXX", { Token: "KSM" }],
-      ["5EYCAe5ViNAoHnU1ZZAJu3bwfpqQ7zTeBZ4VWBYvzNsXugiH", { Token: "KSM" }],
+      ["eCSrvaystgdffuJxPVYKf8H8UYnHGNRdVGUvj1SWSiatWMq", { Token: "KSM" }],
+      ["eCSrvaystgdffuJxPVU9u7Vv2AVjXTAEwbuujLggS6t6HoE", { Token: "KSM" }],
+      ["eCSrvaystgdffuJxPVTJc2eQMgp9PnuPh7mMaQ6KbTynFRM", { Token: "KSM" }],
+      ["eCSrvaystgdffuJxPVZUZmUBqiz2nXKWuUWHQBPqvJFeDh1", { Token: "KSM" }],
+      ["eCSrvaystgdffuJxPVW4UMxAXMuTpU3jkCJeiqppyfoi6SG", { Token: "KSM" }],
     ],
   },
   {
@@ -62,26 +55,8 @@ const DexTokens = [
     currency: { Token: "ZLK" },
     decimal: 18,
     dex_address: [
-      ["5EYCAe5ViNAoHnU1ZZAKfoyy3zaPgXQF31qreBoKBD5ZuYrM", { Token: "ZLK" }],
-    ],
-  },
-  {
-    id: "BNC-dex",
-    coin_id: "bifrost-native-coin",
-    currency: { Native: "BNC" },
-    decimal: 12,
-    dex_address: [
-      "5EYCAe5ViNAoHnU1ZZAKfoyy3zaPgXQF31qreBoKBD5ZuYrM",
-      "5EYCAe5ViNAoHnU1ZZAR51LmTJsa1PC4JmRES1LEWhhomrXX",
-    ],
-  },
-  {
-    id: "kUSD-dex",
-    coin_id: "tether",
-    currency: { Stable: "KUSD" },
-    decimal: 12,
-    dex_address: [
-      ["5EYCAe5ViNAoHnU1ZZAJu3bwfpqQ7zTeBZ4VWBYvzNsXugiH", { Stable: "KUSD" }],
+      ["eCSrvaystgdffuJxPVRWqnxeKZJ3dWu8qJYidgLLStXXkiG", { Token: "ZLK" }],
+      ["eCSrvaystgdffuJxPVU5NQfnXRohvjWF9u8VaeUWRg1mn1y", { Token: "ZLK" }],
     ],
   },
 ];
@@ -141,33 +116,23 @@ export async function handleBlock(block: SubstrateBlock): Promise<void> {
       // JSON.parse(JSON.stringify(tokens_accounts_result)).forEach(item =>{
       //   token_total_issuance.plus(item.free)
       // })
-      if (token.id == "BNC-dex") {
-        const tokens_accounts_result = await Promise.all(
-          token.dex_address.map(async (item) => {
-            const result = await api.query.system.account(item);
-            return result;
-          })
-        );
-        JSON.parse(JSON.stringify(tokens_accounts_result)).forEach((item) => {
-          token_total_issuance = new BigNumber(
-            BigInt(item.data.free).toString()
+
+      const tokens_accounts_result = await Promise.all(
+        token.dex_address.map(async (item) => {
+          const result = await api.query.tokens.accounts(
+            JSON.parse(JSON.stringify(item))[0],
+            JSON.parse(JSON.stringify(item))[1]
           );
-        });
-      } else {
-        const tokens_accounts_result = await Promise.all(
-          token.dex_address.map(async (item) => {
-            const result = await api.query.tokens.accounts(
-              JSON.parse(JSON.stringify(item))[0],
-              JSON.parse(JSON.stringify(item))[1]
-            );
-            return result;
-            // token_total_issuance.plus(BigInt(JSON.parse(JSON.stringify(result)).free).toString())
-          })
-        );
-        JSON.parse(JSON.stringify(tokens_accounts_result)).forEach((item) => {
-          token_total_issuance = new BigNumber(BigInt(item.free).toString());
-        });
-      }
+          return result;
+          // token_total_issuance.plus(BigInt(JSON.parse(JSON.stringify(result)).free).toString())
+        })
+      );
+      JSON.parse(JSON.stringify(tokens_accounts_result)).forEach((item) => {
+        token_total_issuance = new BigNumber(BigInt(item.free).toString())
+          .multipliedBy(2)
+          .plus(token_total_issuance);
+      });
+
       record.total_issuance = BigInt(token_total_issuance.toFixed());
       record.usd = parseFloat(token_price.usd);
       record.cny = parseFloat(token_price.cny);
