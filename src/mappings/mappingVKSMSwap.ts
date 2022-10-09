3d2e292ee81c5f8b8e1dd393bc7ec9758da495d5
killbond007 <chenchengalex2014@gmail.com>
2022-10-09 13:51:56 +0200

    fix: remove kusd

--- src/mappings/mappingVKSMSwap.ts
+++ src/mappings/mappingVKSMSwap.ts
@@ -94,23 +94,23 @@ export async function handleVKSMSwap(event: SubstrateEvent): Promise<void> {
 
         await entity.save();
 
-        if (isKUSD_KSM) {
-          const entity = new VtokenSwapRatio("kUSD_KSM");
-
-          entity.block_height = blockNumber;
-          entity.block_timestamp = event.block.timestamp;
-          entity.event_id = event.idx;
-          entity.asset_0 = swap_path_obj[key];
-          entity.asset_1 = swap_path_obj[key + 1];
-          entity.asset_0_name = asset0.name;
-          entity.asset_1_name = asset1.name;
-          entity.balance_in = balances_obj[key];
-          entity.balance_out = balances_obj[key + 1];
-          entity.ratio = new BigNumber(balances_obj[key + 1].toString())
-            .div(balances_obj[key].toString())
-            .toString();
-          await entity.save();
-        }
+        // if (isKUSD_KSM) {
+        //   const entity = new VtokenSwapRatio("kUSD_KSM");
+        //
+        //   entity.block_height = blockNumber;
+        //   entity.block_timestamp = event.block.timestamp;
+        //   entity.event_id = event.idx;
+        //   entity.asset_0 = swap_path_obj[key];
+        //   entity.asset_1 = swap_path_obj[key + 1];
+        //   entity.asset_0_name = asset0.name;
+        //   entity.asset_1_name = asset1.name;
+        //   entity.balance_in = balances_obj[key];
+        //   entity.balance_out = balances_obj[key + 1];
+        //   entity.ratio = new BigNumber(balances_obj[key + 1].toString())
+        //     .div(balances_obj[key].toString())
+        //     .toString();
+        //   await entity.save();
+        // }
 
         if (isVKSM_KSM) {
           const entity = new VtokenSwapRatio("vKSM_KSM");
